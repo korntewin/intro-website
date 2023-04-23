bf483437944a673b403a22f844bf3cd63ea1236a
korntewin <korntewin.b@gmail.com>
2023-04-23 17:01:24 +0700

    fix: minor typo

--- pages/about.tsx
+++ pages/about.tsx
@@ -14,7 +14,7 @@ function About() {
             <NavBar addClass="z-40 sticky top-0" />
             <div className="flex flex-col items-center h-screen">
                 <p className="mx-20 mt-10 text-justify text-2xl text-white font-semibold drop-shadow">
-                    I&apos;m a self-starter with a willingness to learn, exhibited by proven self-taught skills in Engineergin with GCP&apos;s Professional Data Enginner and GCP&apos;s Professional Data Architect certificate, Deep Learning with Tensorflow Developer Certificate and contribution to Tensorflow Organization codebase.
+                    I&apos;m a self-starter with a willingness to learn, exhibited by proven self-taught skills in Engineering with GCP&apos;s Professional Data Enginner and GCP&apos;s Professional Data Architect certificate, Deep Learning with Tensorflow Developer Certificate and contribution to Tensorflow Organization codebase.
                 </p>
                 <p className="mx-20 mt-5 text-justify text-2xl text-white font-semibold drop-shadow">
                     I am currently working at Thinking Machines as a Machine Learning Engineer, with experience in developing, building, deploying, and monitoring ML model (scikit-learn/tensorflow) and data processing pipeline (Spark on hadoop ecosystem) in production. I have experience working with data processing systems for big data of multiple TB scale with Spark on both batch/online channel.
