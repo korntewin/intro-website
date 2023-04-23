ce34d89bfeb86296fc3c49b9c7a88dddf55fbddd
korntewin <korntewin.b@gmail.com>
2023-04-23 16:50:48 +0700

    fix: remove target blank to force path at client side

--- components/sidebar/sidebar.component.tsx
+++ components/sidebar/sidebar.component.tsx
@@ -15,7 +15,7 @@ export default function SideBar({ ls }: Props) {
         <aside className="z-0 hidden md:block sticky top-[4rem] left-0 h-screen px-4 py-3 overflow-y-auto">
             {ls.map(({ name, href }) => {
                 return (
-                    <Link href={href} target="_blank" key={`sidebar-${name}-${href}`}>
+                    <Link href={href} key={`sidebar-${name}-${href}`}>
                         <h1
                             className="text-white text-lg block px-4 py-3 font-semibold leading-none hover:bg-green-300 hover:font-bold rounded-xl"
                         >{name}</h1>
