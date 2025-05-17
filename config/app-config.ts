ba8bf4de068f68b52be6a0f237ba9dfaeb091862
korntewin <korntewin.b@gmail.com>
2025-05-17 21:49:32 +0700

    config: change rotating interval

--- config/app-config.ts
+++ config/app-config.ts
@@ -4,10 +4,9 @@
 
 export const AppConfig = {
   showcases: {
-    autoRotateIntervalMs: 6000, // Auto-rotate interval in milliseconds
-    progressUpdateIntervalMs: 30, // Progress bar update interval in milliseconds
+    autoRotateIntervalMs: 4500, // Auto-rotate interval in milliseconds
+    progressUpdateIntervalMs: 15, // Progress bar update interval in milliseconds
   },
-  // Add other app-wide configuration sections as needed
 
   highlightedProjects: [
     {
