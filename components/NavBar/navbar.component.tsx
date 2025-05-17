fc424cec91a24ef68abcb22ef40b221e4c9df7a9
korntewin <korntewin.b@gmail.com>
2025-05-17 10:55:40 +0700

    feature: add mock homepage

---
+++ components/NavBar/navbar.component.tsx
@@ -0,0 +1,22 @@
+import styles from "./navbar.module.css";
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.navContainer}>
+                <ul className={styles.navLinks}>
+                    <li className={styles.navItem}>
+                        <Link href="/" className={styles.navLink}>Home</Link>
+                    </li>
+                    <li className={styles.navItem}>
+                        <Link href="/about" className={styles.navLink}>About</Link>
+                    </li>
+                    <li className={styles.navItem}>
+                        <Link href="/projects" className={styles.navLink}>Projects</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
