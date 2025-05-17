ab19175f96e9ba4ac450ff9a6b576b7bfd31982b
korntewin <korntewin.b@gmail.com>
2025-05-17 12:02:28 +0700

    chore: refactor svg icon

--- components/ShowCasesV2/showcasesv2.component.tsx
+++ components/ShowCasesV2/showcasesv2.component.tsx
@@ -3,7 +3,9 @@
 import { useState, useEffect, useCallback, useRef } from "react";
 import styles from "./showcasesv2.module.css";
 import Image from "next/image";
+
 import { AppConfig } from "@/config/app-config";
+import { ArrowLeft, ArrowRight, FollowLink } from "./showcasesv2.icon";
 
 export type ShowCaseV2Data = {
   id: string;
@@ -127,19 +129,7 @@ export default function ShowCasesV2({
           onClick={goToPrevious}
           aria-label="Previous project"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <polyline points="15 18 9 12 15 6"></polyline>
-          </svg>
+         <ArrowLeft />
         </button>
 
         <div className={styles.showcaseContent}>
@@ -176,21 +166,7 @@ export default function ShowCasesV2({
                 rel="noopener noreferrer"
               >
                 View Project
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                  <polyline points="15 3 21 3 21 9"></polyline>
-                  <line x1="10" y1="14" x2="21" y2="3"></line>
-                </svg>
+                <FollowLink />
               </a>
             )}
           </div>
@@ -201,19 +177,7 @@ export default function ShowCasesV2({
           onClick={goToNext}
           aria-label="Next project"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <polyline points="9 18 15 12 9 6"></polyline>
-          </svg>
+            <ArrowRight />
         </button>
       </div>
 
