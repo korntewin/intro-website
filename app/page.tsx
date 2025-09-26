53606e106fa8b4a9b6f204e10e6df9f5e7279f1b
korntewin <korntewin.b@gmail.com>
2025-09-26 08:11:03 +0700

    init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,95 @@
-"use client";
-
+import Image from "next/image";
 import styles from "./page.module.css";
-import ShowCasesV2 from "@/components/ShowCasesV2/showcasesv2.component";
-import Hero from "@/components/Hero/hero.component";
-import About from "@/components/About/about.component";
-import Skills from "@/components/Skills/skills.component";
-import Contact from "@/components/Contact/contact.component";
-import { AppConfig } from "@/config/app-config";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.page}>
       <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol>
+          <li>
+            Get started by editing <code>app/page.tsx</code>.
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
 
-        <section className={styles.section} id="projects">
-          <h2 className={styles.sectionTitle}>Featured Projects</h2>
-          <ShowCasesV2 highlights={AppConfig.highlightedProjects} />
-        </section>
-
-        <Hero />
-        <About />
-        <Skills />
-        <Contact />
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.logo}
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+            Read our docs
+          </a>
+        </div>
       </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === 'production';
-const repositoryName = 'intro-website'; 
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  images: {
-    unoptimized: true
-  },
-  basePath: isProd ? `/${repositoryName}` : "",
-  assetPrefix: isProd ? `/${repositoryName}/` : "",
 };
 
 export default nextConfig;
